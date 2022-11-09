aa21257b81aab3b827f55af88b3994523bd868c3
Hongsi-Taste <kwonwk123@gmail.com>
2022-11-09 16:13:51 +0900

    Image reading & multithreading implement 1

--- main.py
+++ main.py
@@ -3,31 +3,56 @@
 import cv2
 from djitellopy import Tello
 import numpy
+from threading import Thread
 from utils import *
+
+def initializeVideo(drone, sceneCounter):
+    frame_read = drone.get_frame_read().frame  # video on
+
+    height, width, _ = frame_read.frame.shape
+    today = datetime.today().strfttime("%m%d-%H%M%S")
+    video = today + sceneCounter + '.mp4'
+    write_video = cv2.VideoWriter(video, cv2.VideoWriter_fourcc(*'XVID'), 45, (width, height))
+
+    return frame_read, write_video
+
+def saveVideo(frame_read, write_video, keep_record):
+    while keep_record:
+        write_video.write(frame_read.frame)
+        time.sleep(1 / 45)
+
+def video_stream(drone):
+    cv2.namedWindow("Drone Feed")
+    frame_read_f = drone.get_frame_read()  # video on
+    img = frame_read_f.frame
+    cv2.imshow("Drone Feed", img)
+    cv2.waitKey(1)
 
 
 if __name__ == '__main__':
 
     myDrone = initTello() #initialize
-    myDrone.streamon() #cam On
 
-    cv2.namedWindow("Camera Feed")
-    frame_read = myDrone.get_frame_read() #video on
+    th1 = Thread(target=video_stream(), args=myDrone) #drone feed on
+    th1.start()
+
+    sceneCounter = 1
+    keep_record = True
 
     time.sleep(10)
+
     while True:
-        img = frame_read.frame
-        cv2.imshow("Camera Feed", img)
 
         keyboard = cv2.waitKey(1)
         if keyboard & 0xff == ord('q'): #종료 q
-            myDrone.land()
-            frame_read.stop()
-            myDrone.streamoff()
+            # myDrone.land()
+            # frame_read.stop()
+            # myDrone.streamoff()
             exit(0)
             break
-        if keyboard & 0xff == ord('c'): #촬영 시작 c
-            myDrone.takeoff()
-            myDrone.move_up(150)
-            cv2.waitKey(10)
-            #타겟 인식이 들어갈 자리
+        # if keyboard & 0xff == ord('c'): #촬영 시작 c
+        #     myDrone.takeoff()
+        #     myDrone.move_up(150)
+        #     cv2.waitKey(10)
+        #     #타겟 인식이 들어갈 자리
+        #     waitKey(1)
--- utils.py
+++ utils.py
@@ -1,6 +1,7 @@
 #Drone and Robotics Team_5B
 import cv2
 import time
+from datetime import datetime
 from djitellopy import Tello
 
 def initTello():
@@ -8,18 +9,17 @@ def initTello():
 
     myDrone.connect()
 
-    myDrone.for_back_velocity = 0
-    myDrone.left_right_velocity = 0
-    myDrone.up_down_velocity = 0
-    myDrone.yaw_velocity = 0
-    myDrone.speed = 0
+    myDrone.for_back_velocity = 5
+    myDrone.left_right_velocity = 5
+    myDrone.up_down_velocity = 5
+    myDrone.yaw_velocity = 5
+    myDrone.speed = 5
 
     print("\n * Drone Battery Percentage : " + str(myDrone.get_battery()) + "%")
 
     return myDrone
 
 # def resetCoordinate(drone):
-
 
 def scene1(drone): #장면1, 정면에서 후면으로
 
@@ -49,14 +49,19 @@ def scene4(drone): #장면4, 좌측 후면에서 정면으로
 
 def toInitial(drone):
     drone.move_up(200)
-    drone.go(0,300,200,10)
+    drone.go(0, 300, 200, 10)
 
 
 #타겟 초반 설정. 1. 초반에 카메라를 사람앞에 적절한 위치에 두기(4면 / 8면 촬영), 2. 형광조끼, 3. 크롭 이미지 미리 삽입
 def getTarget(drone):
+    drone.cw(360)
 
 #타겟 찾는 과정
 def findTarget(drone):
+    drone.ccw(45)
+    drone.cw(90)
+    drone.ccw(45)
 
-def recandSave(img):
+# def recordandSave(img):
+
 
